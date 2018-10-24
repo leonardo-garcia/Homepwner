1ff1e5a99d93b768ec66812c08735eff377e7ead
Leonardo Garcia <leo.equih@gmail.com>
2018-10-23 23:03:31 -0500

    Create Item object and initializer

---
+++ Homepwner/ItemsViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ItemsViewController.swift
+//  Homepwner
+//
+//  Created by Leonardo Adolfo Garcia E. on 10/21/18.
+//  Copyright © 2018 Equih. All rights reserved.
+//
+
+import UIKit
+
+class ItemsViewController: UITableViewController {
+    
+}
